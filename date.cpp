95796a519f961dbdbfdc0faddfc11e7341ff1104
NorSaH77 <nguyenvhoang0707@gmail.com>
2024-12-28 13:58:54 +0700

    add file date

---
+++ date.cpp
@@ -0,0 +1,188 @@
+#include<iostream>
+
+using namespace std;
+
+class date{
+    private:
+        int ngay, thang, nam;
+    public:
+        date(){}
+
+        date(int d, int m, int y): ngay(d), thang(m), nam(y){}
+
+        ~date(){}
+
+        void nhap(){
+            cout << "Nhap ngay : ";
+            cin >> ngay;
+            cout << "Nhap thang : ";
+            cin >> thang;
+            cout << "Nhap nam : ";
+            cin >> nam;
+        }
+
+        void xuat(){
+            if(ngay < 10 && thang < 10)
+            cout << "0" << ngay << "-" << "0" << thang << "-" << nam << endl;
+            else if(ngay < 10)
+            cout << "0" << ngay << "-" << thang << "-" << nam << endl;
+            else if (thang < 10)
+            cout << ngay << "-" << "0" << thang << "-" << nam << endl;
+            else
+            cout << ngay << "-" << thang << "-" << nam << endl;            
+        }
+
+        void check(){
+            //nam nhuan
+            if(nam % 400 == 0 && nam > 0 && nam <= 2024){
+                //thang co 29 ngay
+                if(thang == 2){
+                    if(ngay > 29 || ngay <= 0){
+                        cout << "Vui long nhap lai!!!" << endl;
+                        return nhap();
+                    }
+                }
+                //thang co 31 ngay
+                else if(thang == 1 || thang == 3 || thang == 5 || thang == 7 || thang == 8 || thang == 10 || thang == 12){
+                    if(ngay > 31 || ngay <= 0){
+                        cout << "Vui long nhap lai!!!" << endl;
+                        return nhap();
+                    }
+                }
+                //thang bthg
+                else{
+                    if(ngay > 30 || ngay <= 0){
+                        cout << "Vui long nhap lai!!!" << endl;
+                        return nhap();
+                    }
+                }
+            }
+            //nam khong nhuan
+            else if(nam > 0 && nam <= 2024){
+                //thang co 31 ngay
+                if(thang == 1 || thang == 3 || thang == 5 || thang == 7 || thang == 8 || thang == 10 || thang == 12){
+                    if(ngay > 31 || ngay <= 0){
+                        cout << "Vui long nhap lai!!!" << endl;
+                        return nhap();
+                    }
+                }
+                //thang co 28 ngay
+                else if(thang == 2){
+                    if(ngay > 28 || ngay <= 0){
+                        cout << "Vui long nhap lai!!!" << endl;
+                        return nhap();
+                    }
+                }
+                //thang bthg
+                else{
+                    if(ngay > 30 || ngay <= 0){
+                        cout << "Vui long nhap lai!!!" << endl;
+                        return nhap();
+                    }
+                }
+            }else{
+                cout << "Vui long nhap lai!!!" << endl;
+                return nhap();
+            }
+        }
+
+        friend date operator ++ (date& dt);
+        friend date operator -- (date& dt);
+};
+
+date operator ++ (date& dt){
+    date temp = dt;
+    //nam nhuan
+    if(dt.nam % 400 == 0){
+        //thang 29 ngay
+        if(dt.thang == 2){
+            if(dt.ngay == 29){
+                dt.ngay = 1;
+                dt.thang++;
+            }
+            else{
+                dt.ngay++;
+            }
+        }
+        //thang 31 ngay
+        else if(dt.thang == 1 || dt.thang == 3 || dt.thang == 5 || dt.thang == 7 || dt.thang == 8 || dt.thang == 10){
+            if(dt.ngay == 31){
+                dt.ngay = 1;
+                dt.thang++;
+            }
+            else{
+                dt.ngay++;
+            }
+        }
+        //thang cuoi nam
+        else if(dt.thang == 12 && dt.ngay == 31){
+            dt.ngay = 1;
+            dt.thang = 1;
+            dt.nam++;
+        }
+        //thang bthg
+        else{
+            if(dt.ngay == 30){
+                dt.ngay = 1;
+                dt.thang++;
+            }
+            else{
+                dt.ngay++;
+            }
+        }
+    }
+    //nam k nhuan
+    else{
+        //thang 28 ngay
+        if(dt.thang == 2){
+            if(dt.ngay == 28){
+                dt.ngay = 1;
+                dt.thang++;
+            }
+            else{
+                dt.ngay++;
+            }
+        }
+        //thang 31 ngay
+        else if(dt.thang == 1 || dt.thang == 3 || dt.thang == 5 || dt.thang == 7 || dt.thang == 8 || dt.thang == 10){
+            if(dt.ngay == 31){
+                dt.ngay = 1;
+                dt.thang++;
+            }
+            else{
+                dt.ngay++;
+            }
+        }
+        //thang cuoi nam
+        else if(dt.thang == 12 && dt.ngay == 31){
+            dt.ngay = 1;
+            dt.thang = 1;
+            dt.nam++;
+        }
+        //thang bthg
+        else{
+            if(dt.ngay == 30){
+                dt.ngay = 1;
+                dt.thang++;
+            }
+            else{
+                dt.ngay++;
+            }
+        }
+    }
+    return dt;
+};
+
+date operator -- (date& dt){
+
+};
+
+int main(){
+    date dt;
+    dt.nhap();
+    dt.check();
+    dt.xuat();
+    ++dt;
+    dt.xuat();
+    return 0;
+}
